bfe04089fcbaf7effa42a9c90eaaf86b26d54558
Jeremy D <emailjcd@gmail.com>
2022-03-12 21:29:20 +0000

    Trace recording completed
    We can now run this script to record the full trace of given remote command

--- testbed.py
+++ testbed.py
@@ -1,14 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3 -u
 from RPi import GPIO
+import time
 
 receiver = 5
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(receiver, GPIO.IN) # Receiver is on GPIO5/pin 29?
 GPIO.setup(13, GPIO.OUT) # Transmitter is on GPIO13/pin33
 
+# Setup
+prev_time = time.perf_counter()
+prev_pin_state = GPIO.input(receiver)
+# If we just want to record the waveform, don't output intitial state?
+# This could miss if the waveform starts with the pullup state but thats very unlikely
+# print(0.0, prev_pin_state)
+
+# Loop
 try:
     while True:
-        print(GPIO.input(receiver))
+        current_pin_state = GPIO.input(receiver)
+        # Only record toggles!
+        if current_pin_state == prev_pin_state:
+            prev_pin_state = current_pin_state
+            continue
+        
+        current_time = time.perf_counter()
+        elapsed_sec = (current_time - prev_time)
+
+        print(elapsed_sec, current_pin_state)
+        prev_pin_state = current_pin_state
+        # Let's do time between, not time overall
+        prev_time = current_time
 except KeyboardInterrupt:
     GPIO.cleanup()
 
