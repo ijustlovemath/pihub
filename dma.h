9303ebdc2c3216d019e12ec0ad28fa6a0cd9c0bc
Jeremy D <emailjcd@gmail.com>
2022-03-15 01:39:49 +0000

    Added necessary DMA code (only dma.h absolutely required)

---
+++ dma.h
@@ -0,0 +1,33 @@
+#pragma once
+
+// Raspberry Pi 2 or 1 ? Since this is a simple example, we don't
+// bother auto-detecting but have it a compile-time option.
+#ifndef PI_VERSION
+#  define PI_VERSION 2
+#endif
+
+#define BCM2708_PI1_PERI_BASE  0x20000000
+#define BCM2709_PI2_PERI_BASE  0x3F000000
+#define BCM2711_PI4_PERI_BASE  0xFE000000
+
+// --- General, Pi-specific setup.
+#if PI_VERSION == 1
+#  define PERI_BASE BCM2708_PI1_PERI_BASE
+#elif PI_VERSION == 2 || PI_VERSION == 3
+#  define PERI_BASE BCM2709_PI2_PERI_BASE
+#else
+#  define PERI_BASE BCM2711_PI4_PERI_BASE
+#endif
+
+#define PAGE_SIZE 4096
+
+// ---- GPIO specific defines
+#define GPIO_REGISTER_BASE 0x200000
+#define GPIO_SET_OFFSET 0x1C
+#define GPIO_CLR_OFFSET 0x28
+#define PHYSICAL_GPIO_BUS (0x7E000000 + GPIO_REGISTER_BASE)
+
+static inline void initialize_gpio_for_output(volatile uint32_t *gpio_registerset, int bit) {
+  *(gpio_registerset+(bit/10)) &= ~(7<<((bit%10)*3));  // prepare: set as input
+  *(gpio_registerset+(bit/10)) |=  (1<<((bit%10)*3));  // set as output.
+}
